8dc938c97bb619688d9c84cdf4bc77ff999946f1
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-06-30 17:33:47 +0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -18,18 +18,7 @@ import JLinkActions from "./components/JLinkActions";
 import { invoke } from "@tauri-apps/api/core";
 
 // Demo firmware files when no folder is selected
-const DEMO_FIRMWARE_FILES = [
-  "di4la-3-1_12-1_x-gd32f330k8u6.hex",
-  "di4la-3-1_11-2_x-gd32f330k8u6.hex",
-  "di4la-2-2_0-1_x-gd32f330k8u6.hex",
-  "dila-4-1_12-1_x-gd32f330c8t6.hex",
-  "di4la-4-1_11-2_x-gd32f330c8t6.hex",
-  "stlink-1-1_5-1_x-stm32f103c8.hex",
-  "stlink-1-1_5-1_x-stm32f103c8t6.hex",
-  "stlink-1-1_5-1_x-stm32f103c8t6.hex",
-  "stlink-1-1_5-3_x-stm32f103c8t6.hex",
-  "stlink-1-1_4-2_x-stm32f103c8t6.hex",
-];
+const DEMO_FIRMWARE_FILES = [""];
 
 function App() {
   const [selectedFirmware, setSelectedFirmware] = useState<FirmwareFile | null>(null);
